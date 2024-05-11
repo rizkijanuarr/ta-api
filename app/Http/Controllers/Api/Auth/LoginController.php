5ec0788aa521ad9f0970e710859431cfc8a72d74
rizkijanuarr <bukanrizkijanuar@gmail.com>
2024-05-11 23:15:45 +0700

    feat: Restful API : Login, Logout

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    // AUTH > LOGIN CONTROLLER
+
+    // LOGIN
+    public function index(Request $request)
+    {
+        //set validasi
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|email',
+            'password' => 'required',
+        ]);
+
+        //response error validasi
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //get "email" dan "password" dari input
+        $credentials = $request->only('email', 'password');
+
+        //check jika "email" dan "password" tidak sesuai
+        if(!$token = auth()->guard('api')->attempt($credentials)) {
+
+            //response login "failed"
+            return response()->json([
+                'success' => false,
+                'message' => 'Email or Password is incorrect'
+            ], 400);
+
+        }
+
+        //response login "success" dengan generate "Token"
+        return response()->json([
+            'success'       => true,
+            'user'          => auth()->guard('api')->user()->only(['name', 'email']),
+            'permissions'   => auth()->guard('api')->user()->getPermissionArray(),
+            'token'         => $token
+        ], 200);
+    }
+
+    // LOGOUT
+    public function logout()
+    {
+        //remove "token" JWT
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        //response "success" logout
+        return response()->json([
+            'success' => true,
+        ], 200);
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// LOGIN
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'index']);
+
+// GROUP WITH MIDDLEWARE "auth"
+Route::group(['middleware' => 'auth:api'], function() {
+
+    // LOGOUT
+    Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
+
 });
